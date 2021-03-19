411ac92cdd40178d64d5186a2deb09549d308725
Rodrigo Rech <rodrigorech@hotmail.com>
2021-03-19 12:48:16 -0300

    add modelos ao trabalho 1
    Adicionado dois modelos ao código fonte.

--- trabalho-1/js/trab1.js
+++ trabalho-1/js/trab1.js
@@ -9,7 +9,33 @@ var init = function () {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    camera.position.z = 100;
+
+    criaAnel();
+    criaCone();
+
     renderer.render(scene, camera);
 }
 
 window.onload = this.init
+
+var criaAnel = function () {
+    const geometria = new THREE.TorusGeometry(5, 2, 8, 24);
+
+    const material = new THREE.MeshBasicMaterial({color: "blue"});
+
+    let ModeloAnel = new THREE.Mesh(geometria, material);
+
+    scene.add(ModeloAnel)
+}
+
+var criaCone = function () {
+    const geometria = new THREE.ConeGeometry(6, 10, 50);
+
+    const material = new THREE.MeshBasicMaterial({color: "red"});
+
+    let hexagono = new THREE.Mesh(geometria, material);
+
+    scene.add(hexagono)
+}
